50e0fe7220aff175dae7ae680ec23a050bbd68c4
go-tiger <dev.gotiger@gmail.com>
2024-11-28 23:29:36 +0900

    feat: 서버에서 플랫폼 중복 연결 허용여부

--- src/main/java/dev/gotiger/donationConnector/config/ConfigManager.java
+++ src/main/java/dev/gotiger/donationConnector/config/ConfigManager.java
@@ -40,4 +40,8 @@ public class ConfigManager {
         }
         return donationConfig;
     }
+
+    public boolean isAllowDuplicatePlatforms(DonationConnector plugin) {
+        return plugin.getConfig().getBoolean("settings.allow-duplicate-platforms", false);
+    }
 }
