e473b9b2edb982ec4d2a3dcbb21b84d4aeaf205b
go-tiger <dev.gotiger@gmail.com>
2024-11-28 23:27:40 +0900

    feat: 후원에 따른 명령어 실행

--- src/main/java/dev/gotiger/donationConnector/config/ConfigManager.java
+++ src/main/java/dev/gotiger/donationConnector/config/ConfigManager.java
@@ -2,6 +2,11 @@ package dev.gotiger.donationConnector.config;
 
 import dev.gotiger.donationConnector.DonationConnector;
 import lombok.Getter;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;;
 
 @Getter
 public class ConfigManager {
@@ -13,5 +18,26 @@ public class ConfigManager {
         plugin.saveDefaultConfig();
 
         streamerManager = new StreamerManager(plugin);
+    }
+
+    public Map<Integer, List<String>> getDonationConfig() {
+        Map<Integer, List<String>> donationConfig = new HashMap<>();
+
+        ConfigurationSection donationSection = plugin.getConfig().getConfigurationSection("Donation");
+        if (donationSection == null) {
+            plugin.getLogger().warning("Donation missing config.yml");
+            return donationConfig;
+        }
+
+        for (String key : donationSection.getKeys(false)) {
+            try {
+                int amount = Integer.parseInt(key);
+                List<String> commands = donationSection.getStringList(key);
+                donationConfig.put(amount, commands);
+            } catch (NumberFormatException e) {
+                plugin.getLogger().warning("Invalid key in Donation section: " + key);
+            }
+        }
+        return donationConfig;
     }
 }
--- src/main/java/dev/gotiger/donationConnector/service/DonationService.java
+++ src/main/java/dev/gotiger/donationConnector/service/DonationService.java
@@ -2,6 +2,11 @@ package dev.gotiger.donationConnector.service;
 
 import dev.gotiger.donationConnector.DonationConnector;
 import dev.gotiger.donationConnector.config.ConfigManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.Map;
 
 public class DonationService {
 
@@ -12,4 +17,26 @@ public class DonationService {
         this.plugin = plugin;
         this.configManager = plugin.getConfigManager();
     }
+
+    public void executeCommands(Player player, Integer payAmount) {
+        Map<Integer, List<String>> donationConfig = configManager.getDonationConfig();
+        List<String> commands = donationConfig.get(payAmount);
+
+        if (commands == null) {
+            commands = donationConfig.get(0);
+        }
+
+        for (String command : commands) {
+            Bukkit.getScheduler().runTask(plugin, () -> {
+                try {
+                    String processedCommand = command.replace("{player}", player.getName());
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), processedCommand);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+        }
+
+    }
+
 }
