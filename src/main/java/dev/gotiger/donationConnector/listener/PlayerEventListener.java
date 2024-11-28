9375b808a0d8743c9729ffe583dd06b01bc2592a
go-tiger <dev.gotiger@gmail.com>
2024-11-28 23:33:08 +0900

    feat: 서버 입장/퇴장 치지직 연결/끊기

--- src/main/java/dev/gotiger/donationConnector/listener/PlayerEventListener.java
+++ src/main/java/dev/gotiger/donationConnector/listener/PlayerEventListener.java
@@ -3,10 +3,15 @@ package dev.gotiger.donationConnector.listener;
 import dev.gotiger.donationConnector.DonationConnector;
 import dev.gotiger.donationConnector.config.ConfigManager;
 import dev.gotiger.donationConnector.service.ChzzkService;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.UUID;
 
 public class PlayerEventListener implements Listener {
     private final DonationConnector plugin;
@@ -21,9 +26,70 @@ public class PlayerEventListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        UUID playerUUID = player.getUniqueId();
+        String playerName = player.getName();
+
+        ConfigurationSection playerStreamerData = configManager.getStreamerManager().isStreamerRegistered(playerUUID);
+
+        if (playerStreamerData != null) {
+            boolean donationLink = playerStreamerData.getBoolean("donationLink", false);
+            if (!donationLink) {
+                player.sendMessage("후원연동 상태: " + ChatColor.RED + "●");
+                return;
+            }
+            boolean allowDuplicatePlatforms = configManager.isAllowDuplicatePlatforms(plugin);
+            String priorityPlatform = playerStreamerData.getString("priority");
+
+            ConfigurationSection platformsSection = playerStreamerData.getConfigurationSection("platforms");
+
+            assert platformsSection != null;
+            String broadcastUUIDChzzk = platformsSection.getString("chzzk");
+            String broadcastUUIDSoop = platformsSection.getString("soop");
+
+            if (allowDuplicatePlatforms) {
+                //todo 모든 플랫폼 연결
+                chzzkService.connectChzzkStreamer(playerUUID, broadcastUUIDChzzk);
+                player.sendMessage("[" + priorityPlatform + "]" + "후원연동 상태: " + ChatColor.GREEN + "●");
+                // Todo 숲 연동 개발 예정
+                // soopService.connectSoopStreamer(playerUUID, broadcastUUIDSoop);
+                // player.sendMessage("[" + priorityPlatform + "]" + "후원연동 상태: " + ChatColor.GREEN + "●");
+            } else {
+                //todo 우선순위 플랫폼만 연결
+                if (priorityPlatform != null) {
+                    switch (priorityPlatform) {
+                        case "chzzk":
+                            chzzkService.connectChzzkStreamer(playerUUID, broadcastUUIDChzzk);
+                            player.sendMessage("[" + priorityPlatform + "]" + "후원연동 상태: " + ChatColor.GREEN + "●");
+                            break;
+                        case "soop":
+                            // Todo 숲 연동 개발 예정
+                            // soopService.connectSoopStreamer(playerUUID, broadcastUUIDSoop);
+                            // player.sendMessage("[" + priorityPlatform + "]" + "후원연동 상태: " + ChatColor.GREEN + "●");
+                            break;
+                    }
+                }
+            }
+
+        } else {
+            ConfigurationSection newStreamerData = configManager
+                    .getStreamerManager()
+                    .getStreamerConfig()
+                    .createSection(playerUUID.toString());
+            newStreamerData.set("nickname", playerName);
+            newStreamerData.set("donationLink", false);
+
+            configManager.getStreamerManager().saveStreamerConfig();
+
+            plugin.getLogger().info("temp player: " + playerName + "("+playerUUID+")");
+        }
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        UUID playerUUID = player.getUniqueId();
+        chzzkService.disconnectChzzkStreamer(playerUUID);
+        plugin.getLogger().info("Disconnected Chzzk API : " + playerUUID);
     }
 }
--- src/main/java/dev/gotiger/donationConnector/service/ChzzkService.java
+++ src/main/java/dev/gotiger/donationConnector/service/ChzzkService.java
@@ -2,15 +2,61 @@ package dev.gotiger.donationConnector.service;
 
 import dev.gotiger.donationConnector.DonationConnector;
 import dev.gotiger.donationConnector.config.ConfigManager;
+import dev.gotiger.donationConnector.network.chzzk.ChzzkAPI;
+import dev.gotiger.donationConnector.network.chzzk.event.implement.*;
+import dev.gotiger.donationConnector.network.chzzk.exception.ChzzkException;
+import dev.gotiger.donationConnector.network.chzzk.listener.ChzzkListener;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ChzzkService {
     private final DonationConnector plugin;
     private final ConfigManager configManager;
     private final DonationService donationService;
+    private ChzzkAPI api;
+    private final Map<UUID, ChzzkAPI> playerApis = new HashMap<>();
 
     public ChzzkService(DonationConnector plugin) {
         this.plugin = plugin;
         this.configManager = plugin.getConfigManager();
         this.donationService = new DonationService(plugin);
+    }
+
+    public void connectChzzkStreamer(UUID playerUUID, String broadcastUUID) {
+        Player player = Bukkit.getPlayer(playerUUID);
+        plugin.getLogger().info(player.getName() + " | " + playerUUID + " | " + broadcastUUID);
+        if (broadcastUUID != null) {
+            try {
+                api = new ChzzkAPI.ChzzkBuilder().withData(broadcastUUID).build()
+                        .addListeners(new ChzzkListener() {
+                            @Override
+                            public void onDonationChat(DonationChatEvent e) {
+                                Integer payAmount = e.getPayAmount();
+                                donationService.executeCommands(player, payAmount);
+                                Bukkit.getLogger().info(player.getName() + "가 " + payAmount + "원을 받았습니다.");
+                            }
+                        });
+                api.connect();
+                playerApis.put(playerUUID, api);
+            } catch (ChzzkException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void disconnectChzzkStreamer(UUID playerUUID) {
+        ChzzkAPI api = playerApis.get(playerUUID);
+        if (api != null) {
+            try {
+                api.disconnect();
+                playerApis.remove(playerUUID);
+            } catch (ChzzkException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
